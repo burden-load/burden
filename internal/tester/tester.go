6c40127b7afe1d616a1fe7a9acafe17d6fb0756a
Vladislav <vlad.kanatik@ya.ru>
2024-11-12 21:12:50 +0300

    add output first metrics, throughput

--- internal/tester/tester.go
+++ internal/tester/tester.go
@@ -2,10 +2,51 @@ package tester
 
 import (
 	"burden/internal/config"
+	"burden/internal/loader"
 	"burden/internal/metrics"
+	"burden/pkg/model"
+	"log"
+	"time"
 )
 
 func RunTest(cfg *config.Config) *metrics.Metrics {
-	// TODO: main functional
-	return metrics.CalculateMetrics()
+	var requests []model.Request
+	var err error
+
+	if cfg.CollectionFile != "" {
+		requests, err = loader.LoadCollection(cfg.CollectionFile)
+		if err != nil {
+			log.Fatalf("Ошибка при загрузке коллекции: %v", err)
+		}
+	} else {
+		requests = []model.Request{
+			{Method: "GET", URL: cfg.URL},
+		}
+	}
+
+	startTime := time.Now()
+	totalRequests := cfg.TotalRequests
+	completedRequests := 0
+
+	for i := 0; i < totalRequests; i++ {
+		// Затычка
+		if sendRequest(requests[i%len(requests)]) {
+			completedRequests++
+		}
+	}
+
+	elapsedTime := time.Since(startTime).Seconds()
+	throughput := float64(completedRequests) / elapsedTime
+
+	log.Printf("Test Completed. Throughput: %.2f RPS", throughput)
+
+	return &metrics.Metrics{
+		Throughput: throughput,
+	}
+}
+
+func sendRequest(req model.Request) bool {
+	// Затычка
+	time.Sleep(10 * time.Millisecond)
+	return true
 }
