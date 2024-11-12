2a8cb8acf2b94fa90df1b0fdcc2ab23fadd4f365
Vladislav <vlad.kanatik@ya.ru>
2024-11-12 21:33:04 +0300

    add working "sendRequest" (request functional)

--- internal/tester/tester.go
+++ internal/tester/tester.go
@@ -6,6 +6,7 @@ import (
 	"burden/internal/metrics"
 	"burden/pkg/model"
 	"log"
+	"net/http"
 	"time"
 )
 
@@ -25,28 +26,57 @@ func RunTest(cfg *config.Config) *metrics.Metrics {
 	}
 
 	startTime := time.Now()
-	totalRequests := cfg.TotalRequests
 	completedRequests := 0
+	var totalResponseTime, totalLatency float64
 
-	for i := 0; i < totalRequests; i++ {
-		// Затычка
-		if sendRequest(requests[i%len(requests)]) {
+	for i := 0; i < cfg.TotalRequests; i++ {
+		start := time.Now()
+		success := sendRequest(requests[i%len(requests)])
+		elapsed := time.Since(start).Seconds()
+
+		if success {
 			completedRequests++
+			totalResponseTime += elapsed
+			totalLatency += elapsed / 2
 		}
 	}
 
 	elapsedTime := time.Since(startTime).Seconds()
 	throughput := float64(completedRequests) / elapsedTime
+	avgResponseTime := totalResponseTime / float64(completedRequests)
+	avgLatency := totalLatency / float64(completedRequests)
 
 	log.Printf("Test Completed. Throughput: %.2f RPS", throughput)
 
 	return &metrics.Metrics{
-		Throughput: throughput,
+		Throughput:   throughput,
+		ResponseTime: avgResponseTime,
+		Latency:      avgLatency,
 	}
 }
 
 func sendRequest(req model.Request) bool {
-	// Затычка
-	time.Sleep(10 * time.Millisecond)
-	return true
+	client := &http.Client{}
+	httpReq, err := http.NewRequest(req.Method, req.URL, nil)
+	if err != nil {
+		log.Printf("Make request failed: %v", err)
+		return false
+	}
+
+	for key, value := range req.Headers {
+		httpReq.Header.Set(key, value)
+	}
+
+	start := time.Now()
+	resp, err := client.Do(httpReq)
+	latency := time.Since(start).Seconds()
+
+	if err != nil {
+		log.Fatalf("Send request failed: %v", err)
+		return false
+	}
+	defer resp.Body.Close()
+
+	log.Printf("Successful request to %s. Latency: %.2f sec, Response code: %d", req.URL, latency, resp.StatusCode)
+	return resp.StatusCode == http.StatusOK
 }
