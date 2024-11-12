563e944aef8d88eea2ff4e106d5d2617133abea8
Vladislav <vlad.kanatik@ya.ru>
2024-11-12 21:04:08 +0300

    add collection load logic

--- internal/loader/loader.go
+++ internal/loader/loader.go
@@ -1,12 +1,31 @@
 package loader
 
 import (
+	"burden/pkg/model"
+	"encoding/json"
+	"io"
+	"log"
 	"os"
 )
 
-func LoadCollection(filePath string) error {
+func LoadCollection(filePath string) ([]model.Request, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
 	}
+	defer file.Close()
+
+	byteValue, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var requests []model.Request
+	if err := json.Unmarshal(byteValue, &requests); err != nil {
+		return nil, err
+	}
+
+	log.Printf("Успешно загружено %d запросов из коллекции", len(requests))
+
+	return requests, nil
 }
