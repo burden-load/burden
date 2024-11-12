519f85d982a199b433bacc7b73e3a300ce7df98e
Vladislav <vlad.kanatik@ya.ru>
2024-11-12 21:14:53 +0300

    add json support for Request struct

--- pkg/model/request.go
+++ pkg/model/request.go
@@ -1,8 +1,8 @@
 package model
 
 type Request struct {
-	Method  string
-	URL     string
-	Body    string
-	Headers map[string]string
+	Method  string            `json:"method"`
+	URL     string            `json:"url"`
+	Body    string            `json:"headers,omitempty"`
+	Headers map[string]string `json:"body,omitempty"`
 }
